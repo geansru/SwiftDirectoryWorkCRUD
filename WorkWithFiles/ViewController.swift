6db11a3ecae025eeb5df50a2ed5d65b18b4cad33
Dmitry Roytman <d@geans.ru>
2015-10-09 16:10:35 +0500

    Add directory listing

--- WorkWithFiles/ViewController.swift
+++ WorkWithFiles/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    var list = [String]()
+    let fileManager = NSFileManager.defaultManager()
+    var currentDir:String?
+    lazy var baseDir: String = {
+       let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        return paths[0]
+    }()
+    // MARK: @IBOutlet
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: @IBAction
+    @IBAction func create(sender: AnyObject) {
+        textField.resignFirstResponder()
+        if textField.text!.isEmpty {
+            print("textField is empty")
+        } else {
 
+            let newDirName = textField.text!
+            currentDir = baseDir + "/" + newDirName
+            if checkCurrentDir() { return }
+            try! fileManager.createDirectoryAtPath(currentDir!, withIntermediateDirectories: true, attributes: nil)
+            checkCurrentDir()
+        }
+    }
+    
+    @IBAction func listDir() {
+        if currentDir != nil {
+            list = try! fileManager.contentsOfDirectoryAtPath(currentDir!)
+        } else {
+            print("Current dir is nil please create/choose current directory.")
+        }
+    }
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Helper
+    func checkCurrentDir() -> Bool {
+        if fileManager.fileExistsAtPath(currentDir!) {
+            print("\(currentDir)")
+            return true
+        } else {
+            print("\(currentDir) is not exists")
+        }
+        return false
     }
-
+    
+    func configureView() {
+        createButton.layer.cornerRadius = 5
+        createButton.layer.masksToBounds = true
+    }
 
 }
 
